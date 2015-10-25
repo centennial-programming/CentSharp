c4aa52d4dcbc30d4d5666d55b74f64652ccf2848
Rai <raimckenzie@users.noreply.github.com>
2015-10-24 17:21:14 -0700

    Setup Form
    Disable resize, fixed form size.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace CentSharp.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap octo {
+            get {
+                object obj = ResourceManager.GetObject("octo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
