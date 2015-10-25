462530e423dba4f23b7043137e3aeb5a4395715c
Rai <raimckenzie@users.noreply.github.com>
2015-10-24 17:37:37 -0700

    Added commenting

--- Form1.cs
+++ Form1.cs
@@ -19,27 +19,34 @@ namespace CentSharp
             InitializeComponent();
         }
 
+		//On Form load (Application Start)
         private void Form1_Load(object sender, EventArgs e)
         {
+			//Register event listeners.
 			Form form1 = this;
 			form1.KeyDown += new KeyEventHandler(Form1_KeyDown);
         }
-           
+          
+		//KeyDown event.
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
 
 			switch (e.KeyCode)
             {
-                case Keys.Up:
+                case Keys.Up: //If UP key is pressed.
+					//Move picture box to new location.
 					octo.Location = new Point(octo.Location.X, octo.Location.Y - step);
 				break;
-				case Keys.Down:
+				case Keys.Down: //If DOWN key is pressed.
+					//Move picture box to new location.
 					octo.Location = new Point(octo.Location.X, octo.Location.Y + step);
 				break;
-				case Keys.Left:
+				case Keys.Left: //If LEFT key is pressed.
+					//Move picture box to new location.
 					octo.Location = new Point(octo.Location.X - step, octo.Location.Y);
 					break;
-				case Keys.Right:
+				case Keys.Right: //If RIGHT key is pressed.
+					 //Move picture box to new location.
 					octo.Location = new Point(octo.Location.X + step, octo.Location.Y);
 				break;
 			}
